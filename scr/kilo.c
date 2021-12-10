0455ffde03a52247dbccb9611e0f3dad4be64b7c
Andy Chen <acchen@cpp.edu>
2021-12-10 02:47:40 +0000

    prints 0 when no input is read

--- scr/kilo.c
+++ scr/kilo.c
@@ -22,6 +22,9 @@ void enableRawMode()
    raw.c_cflag |= (CS8);
    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
+   raw.c_cc[VMIN] = 0;
+   raw.c_cc[VTIME] = 1;
+
    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -30,8 +33,11 @@ int main()
    enableRawMode();
 
    char c;
-   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+   while (1)
    {
+      char c = '\0';
+      read(STDIN_FILENO, &c, 1);
+      
       if (iscntrl(c))
       {
          printf("%d\r\n", c);
@@ -39,6 +45,11 @@ int main()
       else
       {
          printf("%d ('%c')\r\n", c, c);
+      }
+
+      if (c == 'q')
+      {
+         break;
       }
    }
 
