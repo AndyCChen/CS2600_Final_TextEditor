c06976ef7944b2441d7668770e84db8278f175ad
Andy Chen <acchen@cpp.edu>
2021-12-10 05:50:31 +0000

    added window size functions

--- scr/kilo.c
+++ scr/kilo.c
@@ -4,10 +4,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
+
+// defines
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios original_termios;
+// data
+
+struct editorConfig
+{
+   int screenRows;
+   int screenCols;
+   struct termios original_termios;
+};
+
+struct editorConfig E;
 
 // terminal
 
@@ -22,7 +34,7 @@ void die(const char *s)
 
 void disableRawMode()
 {
-   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.original_termios) == -1)
    {
       die("tcsetattr");
    }
@@ -30,13 +42,13 @@ void disableRawMode()
 
 void enableRawMode()
 {
-   if (tcgetattr(STDIN_FILENO, &original_termios) == -1)
+   if (tcgetattr(STDIN_FILENO, &E.original_termios) == -1)
    {
       die("tcgetattr");
    }
    atexit(disableRawMode);
 
-   struct termios raw = original_termios;
+   struct termios raw = E.original_termios;
    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
    raw.c_oflag &= ~(OPOST);
    raw.c_cflag |= (CS8);
@@ -59,6 +71,62 @@ char editorReadKey()
    return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+   char buf[32];
+   unsigned int i = 0;
+
+   if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+   {
+      return -1;
+   }
+
+   while (i < sizeof(buf) - 1)
+   {
+      if (read(STDIN_FILENO, &buf[i], 1) != 1) 
+      {
+         break;
+      }
+      if (buf[i] == 'R')
+      { 
+         break;
+      }
+      i++;
+   }
+   buf[i] = '\0';
+
+   if (buf[0] != '\x1b' || buf[1] != '[') 
+   {
+      return -1;
+   }
+   if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) 
+   {  
+      return -1;
+   }
+
+   return 0;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+   struct winsize ws;
+
+   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
+   {
+      if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      { 
+         return -1;
+      }
+      return getCursorPosition(rows, cols);
+   } 
+   else 
+   {
+      *cols = ws.ws_col;
+      *rows = ws.ws_row;
+      return 0;
+   }
+}
+
 // output
 
 void editorRefreshScreen()
@@ -73,7 +141,7 @@ void editorRefreshScreen()
 void editorDrawRows()
 {
    int y;
-   for (y = 0; y < 24; y++)
+   for (y = 0; y < E.screenRows; y++)
    {
       write(STDOUT_FILENO, "~\r\n", 3);
    }
@@ -95,9 +163,21 @@ void editorProcessKeypress()
   }
 }
 
+// initialize
+
+void initEditor()
+{
+
+   if (getWindowSize(&E.screenRows, &E.screenRows) == -1)
+   {
+      die("getWindowSize");
+   }
+}
+
 int main()
 {
    enableRawMode();
+   initEditor();
 
    char c;
    while (1)
