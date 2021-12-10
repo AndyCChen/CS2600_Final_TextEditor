dd6cb9c0bbde1c335d3b6ae6f30846c6fc160afb
Andy Chen <acchen@cpp.edu>
2021-12-10 06:06:17 +0000

    append the buffer

--- scr/kilo.c
+++ scr/kilo.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 // defines
 
@@ -127,25 +128,62 @@ int getWindowSize(int *rows, int *cols)
    }
 }
 
+// append buffer
+
+struct abuf
+{
+   char *b;
+   int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+   char *new = realloc(ab->b, ab->len + len);
+
+   if (new == NULL)
+   {
+      return;
+   }
+   memcpy(&new[ab->len], s, len);
+   ab->b = new;
+   ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+   free(ab->b);
+}
+
 // output
 
 void editorRefreshScreen()
-{
-   write(STDOUT_FILENO, "\x1b[2J", 4);
-   write(STDOUT_FILENO, "\x1b[H", 3);
+ {
+   struct abuf ab = ABUF_INIT;
 
-   editorDrawRows();
-   write(STDOUT_FILENO, "\x1b[H", 3);
+   abAppend(&ab, "\x1b[2J", 4);
+   abAppend(&ab, "\x1b[H", 3);
+
+   editorDrawRows(&ab);
+
+   abAppend(&ab, "\x1b[H", 3);
+   
+   write(STDOUT_FILENO, ab.b, ab.len);
+   abFree(&ab);
 }
 
-void editorDrawRows() {
+
+void editorDrawRows(struct abuf *ab) 
+{
    int y;
-   for (y = 0; y < E.screenRows; y++) 
+   for (y = 0; y < E.screenRows; y++)
    {
-      write(STDOUT_FILENO, "~", 1);
+
+      abAppend(ab, "~", 1);
       if (y < E.screenRows - 1) 
       {
-         write(STDOUT_FILENO, "\r\n", 2);
+         abAppend(ab, "\r\n", 2);
       }
    }
 }
